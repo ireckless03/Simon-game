ceab0172a1a73932ca7735e2a04d3aaf7a5ab20c
Denny Doan <denny-333@hotmail.com>
2023-09-12 16:23:42 -0400

    Converted to Typescript

---
+++ dist/index.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    let level = 0;
+    const buttonColours = ['green', 'red', 'yellow', 'blue'];
+    let gamePattern = [];
+    let userClickedPattern = [];
+    let gameStarted = false;
+    $(document).keypress(function (event) {
+        if (!gameStarted) {
+            startGame();
+        }
+    });
+    function startGame() {
+        resetGame();
+        nextSequence();
+        gameStarted = true;
+    }
+    function resetGame() {
+        level = 0;
+        gamePattern = [];
+        userClickedPattern = [];
+        $('#level-title').text('Press A Key to Start');
+    }
+    function nextSequence() {
+        userClickedPattern = [];
+        level++;
+        $('#level-title').text('Level ' + level);
+        const randomChosenColour = pickRandomColour();
+        gamePattern.push(randomChosenColour);
+        setTimeout(() => {
+            animateAndPlay(randomChosenColour);
+        }, 1000);
+    }
+    function pickRandomColour() {
+        const randomNumber = Math.floor(Math.random() * buttonColours.length);
+        return buttonColours[randomNumber];
+    }
+    $('.btn').click(function () {
+        const clickedButtonId = $(this).attr('id');
+        userClickedPattern.push(clickedButtonId);
+        animateAndPlay(clickedButtonId);
+        checkAnswer(userClickedPattern.length - 1);
+    });
+    function animateAndPlay(colour) {
+        $('.' + colour)
+            .fadeIn(100)
+            .fadeOut(100)
+            .fadeIn(100);
+        const audio = new Audio('sounds/' + colour + '.mp3');
+        audio.play();
+    }
+    function checkAnswer(currentLevel) {
+        if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+            console.log('Success');
+            if (userClickedPattern.length === gamePattern.length) {
+                setTimeout(function () {
+                    userClickedPattern = [];
+                    nextSequence();
+                }, 1000);
+            }
+        }
+        else {
+            endGame();
+        }
+    }
+    function endGame() {
+        $('#level-title').text('Game Over, Press Any Key to Restart');
+        const gameOver = new Audio('sounds/wrong.mp3');
+        gameOver.play();
+        gameStarted = false;
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,22 @@
 $(document).ready(function () {
-  // Initialize game variables
-  let level = 0; // Current game level
-  const buttonColours = ['green', 'red', 'yellow', 'blue']; // Array of button colors
-  let gamePattern = []; // Stores the generated game pattern
-  let userClickedPattern = []; // Stores the user's clicked pattern
-  let gameStarted = false; // Flag to track if the game has started
+  let level = 0;
+  const buttonColours: string[] = ['green', 'red', 'yellow', 'blue'];
+  let gamePattern: string[] = [];
+  let userClickedPattern: string[] = [];
+  let gameStarted = false;
 
-  // Attach a key press event handler to start the game
   $(document).keypress(function (event) {
     if (!gameStarted) {
       startGame();
     }
   });
 
-  // Start the game
   function startGame() {
     resetGame();
     nextSequence();
     gameStarted = true;
   }
 
-  // Reset the game variables
   function resetGame() {
     level = 0;
     gamePattern = [];
@@ -28,7 +24,6 @@ $(document).ready(function () {
     $('#level-title').text('Press A Key to Start');
   }
 
-  // Generate the next element in the game pattern
   function nextSequence() {
     userClickedPattern = [];
     level++;
@@ -40,13 +35,11 @@ $(document).ready(function () {
     }, 1000);
   }
 
-  // Pick a random color from the buttonColours array
   function pickRandomColour() {
     const randomNumber = Math.floor(Math.random() * buttonColours.length);
     return buttonColours[randomNumber];
   }
 
-  // Handle user button clicks
   $('.btn').click(function () {
     const clickedButtonId = $(this).attr('id');
     userClickedPattern.push(clickedButtonId);
@@ -54,8 +47,7 @@ $(document).ready(function () {
     checkAnswer(userClickedPattern.length - 1);
   });
 
-  // Function to handle animations and audio
-  function animateAndPlay(colour) {
+  function animateAndPlay(colour: string) {
     $('.' + colour)
       .fadeIn(100)
       .fadeOut(100)
@@ -65,8 +57,7 @@ $(document).ready(function () {
     audio.play();
   }
 
-  // Check if the user's answer is correct
-  function checkAnswer(currentLevel) {
+  function checkAnswer(currentLevel: number) {
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
       console.log('Success');
 
@@ -81,7 +72,6 @@ $(document).ready(function () {
     }
   }
 
-  // End the game and reset all variables
   function endGame() {
     $('#level-title').text('Game Over, Press Any Key to Restart');
     const gameOver = new Audio('sounds/wrong.mp3');
